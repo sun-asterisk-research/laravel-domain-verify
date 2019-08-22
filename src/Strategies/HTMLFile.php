06107f518a7701321e06971e29140e537a9134b7
datbnt-0905 <nguyen.tien.datb@sun-asterisk.com>
2019-08-22 15:00:53 +0700

    feat: add HTML File

--- src/Strategies/HTMLFile.php
+++ src/Strategies/HTMLFile.php
@@ -8,15 +8,29 @@ use SunAsterisk\DomainVerifier\DomainVerificationFacade;
 
 class HTMLFile implements StrategyInterface
 {
+    /**
+     * Verify domain ownership via HTML meta tag
+     *
+     * @param string $url
+     * @param DomainVerifiableInterface $domainVerifiable
+     * @return bool
+     */
     public function verify(string $url, DomainVerifiableInterface $domainVerifiable)
     {
-        $verificationName = config("domain_verifier.verification_name");
-        $urlFile = $url.'./'.$verificationName.'.html';
-        $domainToken = @file_get_contents($urlFile);
+        $domainToken = $this->getToken($url);
         if ($domainToken == null) return false;
         $verificationToken = DomainVerificationFacade::getTokenFor($url, $domainVerifiable)->token;
         $n = strlen($verificationToken);
         $domainToken = substr($domainToken, 0, $n);
         return $domainToken == $verificationToken;
     }
+
+    protected function getToken($url)
+    {
+        $verificationName = config("domain_verifier.verification_name");
+        $urlFile = $url.'./'.$verificationName.'.html';
+        $domainToken = @file_get_contents($urlFile);
+        return $domainToken;
+    }
+
 }
