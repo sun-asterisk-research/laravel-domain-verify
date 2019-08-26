7ec0a1fd815fc555d453899780c19a826bfb86b8
vunt-0906 <52224331+vunt-0906@users.noreply.github.com>
2019-08-26 11:03:38 +0700

    feat: verify via Mail (#15)

--- src/Strategies/Mail.php
+++ src/Strategies/Mail.php
@@ -2,8 +2,11 @@
 
 namespace SunAsterisk\DomainVerifier\Strategies;
 
+use Illuminate\Support\Facades\Mail as IlluminateMail;
 use SunAsterisk\DomainVerifier\Contracts\Models\DomainVerifiableInterface;
 use SunAsterisk\DomainVerifier\Contracts\Strategies\StrategyInterface;
+use SunAsterisk\DomainVerifier\DomainVerificationFacade as DomainVerification;
+use SunAsterisk\DomainVerifier\Supports\URL;
 
 class Mail implements StrategyInterface
 {
@@ -16,6 +19,20 @@ class Mail implements StrategyInterface
      */
     public function verify(string $url, DomainVerifiableInterface $domainVerifiable)
     {
-        return false;
+        $token = DomainVerification::getTokenFor($url, $domainVerifiable)->token;
+        $url = URL::getDomainName($url);
+        $view = config('domain_verifier.mail.view');
+        $subject = config('domain_verifier.mail.subject');
+        $data = config('domain_verifier.mail.data');
+        if (!isset($data['token'])){
+            $data['token'] = $token;
+        }
+
+        IlluminateMail::send($view, $data, function ($message) use ($url, $subject) {
+            $message
+                ->to('admin@'.$url, "Admin")
+                ->to('webmaster@'.$url, "Admin")
+                ->subject($subject);
+        });
     }
 }
