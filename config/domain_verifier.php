0fb4db6414ac92d964e4bbc5ce55a3c00ab113cc
vunt-0906 <nguyen.tuan.vu@sun-asterisk.com>
2019-08-26 10:16:57 +0700

    chore: add some attributes to config

--- config/domain_verifier.php
+++ config/domain_verifier.php
@@ -3,4 +3,12 @@
 return [
     // Used to find token:
     'verification_name' => env('DOMAIN_VERIFICATION_NAME', 'domain-verification'),
+
+    'mail' => [
+        'view' => env('DOMAIN_VERIFY_MAIL_VIEW', "domain-verifier::mail"),
+        'subject' => env('DOMAIN_VERIFY_MAIL_SUBJECT', "Domain Verify Acttivation"),
+        'data' => env('DOMAIN_VERIFY_MAIL_DATA')
+    ],
+
+    'activation_route' => env('DOMAIN_VERIFY_ACTIVATION_ROUTE', "domain-verify.activated")
 ];
