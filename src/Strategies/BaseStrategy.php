0ab47b49c47af0f13af1df988b8f77c75b5c025e
tranxuanthang <tran.xuan.thangb@sun-asterisk.com>
2020-07-07 16:22:06 +0700

    refactor: html meta tag strategy

--- src/Strategies/BaseStrategy.php
+++ src/Strategies/BaseStrategy.php
@@ -11,6 +11,13 @@ abstract class BaseStrategy implements StrategyInterface
 {
     abstract public function verify(string $url, DomainVerifiableInterface $domainVerifiable): VerifyResult;
 
+    /**
+     * Get token for verification process
+     *
+     * @param  string  $url
+     * @param  DomainVerifiableInterface  $domainVerifiable
+     * @return string
+     */
     public function getToken(string $url, DomainVerifiableInterface $domainVerifiable): string
     {
         $record = DomainVerificationFacade::firstOrCreate($url, $domainVerifiable);
